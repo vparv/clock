026a1ba65562910853af5017080d660ecccc4c55
Vineet Parvathala <vparv@umich.edu>
2020-07-05 17:27:19 -0400

    added weather

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function moveHands() {
     document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
    
     setTimeout(moveHands, 1000); // calling the function every second
+  
   }
 }
 
@@ -34,4 +35,50 @@ function newItem() {
   document.getElementById("input").value = "";
 }
 
-window.onload = moveHands; // making sure the function starts on load of webpage
+function getWeather() {
+  let temperature = document.getElementById("temperature");
+  let description = document.getElementById("description");
+  let location = document.getElementById("location");
+
+  let api = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "f146799a557e8ab658304c1b30cc3cfd";
+
+  location.innerHTML = "Locating...";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+
+    let url =
+      api +
+      "?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=" +
+      apiKey +
+      "&units=imperial";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "° F";
+        location.innerHTML =
+          data.name + " (" + latitude + "°, " + longitude + "°)";
+        description.innerHTML = data.weather[0].main;
+      });
+  
+    }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
+}
+
+moveHands();
+
+window.onload = getWeather(); // making sure the function starts on load of webpage
